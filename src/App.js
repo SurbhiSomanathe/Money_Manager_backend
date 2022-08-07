6ad8315fb90e4def1b088dc65b502881126ac341
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-08-08 01:06:09 +0530

    FC

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Router from "./components/Router";
+import React from "react";
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
